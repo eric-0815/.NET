888eb838935dcec7aac2f5b6494da83909d68304
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2022-05-10 15:03:29 +1000

    add dummy data for Product

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -13,6 +13,6 @@ namespace API.Data
         {
         }
 
-        public DbSet<Products> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace API.Entities
 {
-    public class Products
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
