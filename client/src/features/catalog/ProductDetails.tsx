e070339886c3960b17c394703378ee60f8456032
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2022-05-15 15:06:59 +1000

    add Loading Component

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -12,6 +12,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -25,8 +27,13 @@ export default function ProductDetails() {
       .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) return <h3>Loading...</h3>;
-  if (!product) return <h3>Product not found</h3>;
+  if (loading) return <LoadingComponent message="Loading product" />;
+  if (!product)
+    return (
+      <h3>
+        <NotFound />
+      </h3>
+    );
   return (
     <Grid container spacing={6}>
       <Grid item xs={6}>
